bb2a6cb37ae6fdaf858e68cce42da24116eb4acd
Reona Oshima(totoraj) <totoraj930@yahoo.co.jp>
2023-03-22 19:08:46 +0900

    更新

--- example/stream.ts
+++ example/stream.ts
@@ -1,4 +1,6 @@
-import { zRaidTweetMini } from '$/redis/schema';
+import { GbsList, zGbsList } from '$/cache/gbsList';
+import { RaidTweetMini, zRaidTweetMini } from '$/redis/schema';
+import axios from 'axios';
 import { WebSocket } from 'ws';
 import { z } from 'zod';
 
@@ -13,7 +15,36 @@ const zTimeMessage = z.object({
 
 const zMessage = z.union([zTweetMessage, zTimeMessage]);
 
+let gbsList: GbsList = [];
+
+/**
+ * ツイートを受信したらコンソールに出力する
+ */
+function onTweet(tweet: RaidTweetMini) {
+  // リストから敵情報を取得
+  const enemy = gbsList.find((item) => item.id === tweet.ei);
+
+  if (!enemy) {
+    // リスト外
+    console.log(
+      tweet.bi,
+      'Lv.' + (tweet.lv ?? '???'),
+      tweet.en,
+      '@' + tweet.sn
+    );
+  } else {
+    // リスト内
+    console.log(tweet.bi, 'Lv.' + enemy.level, enemy.ja, '@' + tweet.sn);
+  }
+}
+
 async function main() {
+  // 敵の情報リスト
+  gbsList = zGbsList.parse(
+    (await axios.get('https://gbs.eriri.net/list')).data
+  );
+
+  // WebSocketに接続
   // const ws = new WebSocket('ws://localhost:10502/all');
   const ws = new WebSocket('wss://gbs-open.eriri.net/api/stream/all');
   ws.on('open', () => {
@@ -22,7 +53,13 @@ async function main() {
   ws.on('message', (rawMsg) => {
     try {
       const msg = zMessage.parse(JSON.parse(rawMsg.toString('utf-8')));
-      console.log(msg);
+      if (msg.type === 't') {
+        // ツイート
+        onTweet(msg.data);
+      } else {
+        // それ以外のメッセージ
+        console.log(msg);
+      }
     } catch {}
   });
 }
